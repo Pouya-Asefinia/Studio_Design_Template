1c387ce86eb8269456f70bc18847e64cb0f79d8f
Pouya <p.asefinia83@gmail.com>
2025-03-10 22:44:07 +0330

    Header last part
    Header is completed

--- SRC/JS/script.js
+++ SRC/JS/script.js
@@ -3,9 +3,14 @@
 
 const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
 const mobileMenu = document.querySelector('.mobile-menu');
+const mobileFilter = document.querySelector('.menu-overlay');
 
-if (mobileMenuBtn && mobileMenu) {
-    mobileMenuBtn.addEventListener('click', () => {
-        mobileMenu.classList.toggle('open-menu');
-    });
-}
+mobileMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.add('open-menu');
+    mobileFilter.classList.add('active-overlay');
+});
+mobileFilter.addEventListener('click', () => {
+    mobileMenu.classList.remove('open-menu');
+    mobileFilter.classList.remove('active-overlay');
+})
+
