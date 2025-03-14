8ed76fdc1c1e43cb3f4c5af1bafda162fdb18932
Pouya <p.asefinia83@gmail.com>
2025-03-15 00:28:37 +0330

    Introdus part
    intrudes part is completed
    and the position of some shape in Help part changed

--- SRC/JS/script.js
+++ SRC/JS/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-
+// mobile navbar
 const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileFilter = document.querySelector('.menu-overlay');
@@ -14,3 +14,19 @@ mobileFilter.addEventListener('click', () => {
     mobileFilter.classList.remove('active-overlay');
 })
 
+// Video in Introdus part 
+const videoPart = document.querySelector('.video-bg');
+const video = document.querySelector('.video');
+const palyVideo = document.querySelector('.video-play-btn');
+const pauseVideo = document.querySelector('.video-pause-btn');
+
+palyVideo.addEventListener('click', () => {
+    palyVideo.classList.add('deactive-btn');
+    videoPart.classList.add('active-btn');
+    video.play();
+})
+pauseVideo.addEventListener('click', () => {
+    palyVideo.classList.remove('deactive-btn');
+    videoPart.classList.remove('active-btn');
+    video.pause();
+})
